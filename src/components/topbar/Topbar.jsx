b3475eb23bcfe0b7a61356abfa64577e1d9e0679
YourMD <gencoalpay@gmail.com>
2021-09-04 16:52:26 +0300

    I was added css files

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./topbar.css"
+
+export default function Topbar() {
+    return (
+        <div className="topbar">
+                <div className="topbarWrapper">
+                    <div className="topleft">
+                        <span className="logo">Nicely Admin</span>
+                    </div>
+                    <div className="topright">
+                        <div className="topbarIcons"></div>
+                    </div>
+                </div>
+            Topbar JSX
+        </div>
+    )
+}
