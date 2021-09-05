6e91684e321658106726613312f4ee6211a44188
YourMD <gencoalpay@gmail.com>
2021-09-05 11:31:12 +0300

    Sidebar was added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import Topbar from "./components/topbar/Topbar"
+import Sidebar from "./components/sidebar/Sidebar";
+import Topbar from "./components/topbar/Topbar";
+import "./app.css";
 
 function App() {
   return (
     <div>
-      <Topbar/>
+      <Topbar />
+      <div className="container">
+          <Sidebar/>
+        
+      </div>
     </div>
   );
 }
