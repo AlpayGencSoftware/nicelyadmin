0e1cadb3c04a5be2f0a75cea248fa0fc08e2f735
YourMD <gencoalpay@gmail.com>
2021-09-05 19:51:33 +0300

    Featured section was uploaded

--- src/components/featuredInfo/FeaturedInfo.jsx
+++ src/components/featuredInfo/FeaturedInfo.jsx
@@ -1,4 +1,5 @@
-import "./featuredInfo.css"
+import "./featuredInfo.css";
+import {ArrowDownward, ArrowUpward} from '@material-ui/icons'; 
 
 export default function FeaturedInfo() {
     return (
@@ -7,8 +8,31 @@ export default function FeaturedInfo() {
                 <span className="featuredTitle">Renavue</span>
                  <div className="featuredMoneyContainer">
                      <span className="featuredMoney">$2,415</span>
-                     <span className="fearuredMoneyRate">$2,415</span>
+                     <span className="fearuredMoneyRate">-11,45<ArrowDownward/></span>
                  </div>
+                 <span className="featuredSub">
+                     Compared to last mounth
+                 </span>
+            </div>
+            <div className="featuredItem">
+                <span className="featuredTitle">Sales</span>
+                 <div className="featuredMoneyContainer">
+                     <span className="featuredMoney">$2,415</span>
+                     <span className="fearuredMoneyRate">-1,45<ArrowDownward/></span>
+                 </div>
+                 <span className="featuredSub">
+                     Compared to last mounth
+                 </span>
+            </div>
+            <div className="featuredItem">
+                <span className="featuredTitle">Cost</span>
+                 <div className="featuredMoneyContainer">
+                     <span className="featuredMoney">$2,415</span>
+                     <span className="fearuredMoneyRate">+5,45<ArrowUpward/></span>
+                 </div>
+                 <span className="featuredSub">
+                     Compared to last mounth
+                 </span>
             </div>
         </div>
     )
