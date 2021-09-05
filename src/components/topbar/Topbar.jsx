9993be0dae29f7b05890b716cdc8c5f652f78799
YourMD <gencoalpay@gmail.com>
2021-09-05 10:36:43 +0300

    Documents were uploaded

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import "./topbar.css"
+import { NotificationsNone, Settings, Language } from '@material-ui/icons';
+ 
 
 export default function Topbar() {
     return (
@@ -8,8 +10,19 @@ export default function Topbar() {
                     <div className="topleft">
                         <span className="logo">Nicely Admin</span>
                     </div>
-                    <div className="topright">
-                        <div className="topbarIcons"></div>
+                    <div className="topRight">
+                        <div className="topbarIconContainer">
+                            <NotificationsNone/>
+                            <span className="topIconBadge">
+                                2
+                            </span>
+                        </div>
+                        <div className="topbarIconContainer">
+                            <Language/>  
+                        </div>
+                        <div className="topbarIconContainer">
+                            <Settings/>
+                        </div>
                     </div>
                 </div>
             Topbar JSX
